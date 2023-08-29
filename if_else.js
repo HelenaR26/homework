84929520732aa68783867ae241ebddeac0962d7e
Elena <you@example.com>
2023-08-29 20:25:18 +0300

    added_multiple_if_else

--- if_else.js
+++ if_else.js
@@ -10,8 +10,18 @@ if (num215 >= 215 && num215 < 250) {
     console.log('Second condition works')
 }
 
+if (Number.isInteger(num215)) {
+    console.log('num is number')
+} else {
+    console.log('code goes to the else')
+}
+
 if (num215 > 300) {
     console.log('num215 variable greater than 300')
+} else if (num215 >= 250) {
+    console.log('num greater than 250')
+} else if (num215 >= 200) {
+    console.log('num greater than 200')
 } else {
     console.log('code goes to the else')
 }
