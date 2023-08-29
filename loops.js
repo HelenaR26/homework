d51d9a90a95d69ece25587c0dab321481f7a5620
Elena <you@example.com>
2023-08-29 20:45:01 +0300

    added_while_loop

--- loops.js
+++ loops.js
@@ -1,2 +1,17 @@
 // the initial changes for wirk with JS
 // author Elena
+
+//while
+let num = 1;
+
+// while (num <= 7) {
+//     console.log(num)
+//     num++
+// }
+
+
+//do while
+// do {
+//     num--
+//     console.log(num)
+// } while (num <= 7)
