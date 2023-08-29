b1ed4e1b4ed02630994445bfa7e66cc21a1d278d
Elena <you@example.com>
2023-08-29 21:18:46 +0300

    added_for_in_of_to_loops

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 // the initial changes for wirk with JS
 // author Elena
 
-//while
-let num = 10;
+// //while
+// let num = 10;
 
 // while (num <= 7) {
 //     console.log(num)
@@ -18,19 +18,50 @@ let num = 10;
 
 //for
 
-let sumOfLoop = 0;
+// let sumOfLoop = 0;
 // for (let i = 0; i <= num; i+=2) {
 //     console.log(i);
 //     if (i == 6) break;
 //     sumOfLoop += i;
 // };
 
-for (let i = 0; i <= num; i+=2) {
-    // if (i == 6) continue;
-    console.log(i);
-    sumOfLoop += i;
-};
+// for (let i = 0; i <= num; i+=2) {
+//     // if (i == 6) continue;
+//     console.log(i);
+//     sumOfLoop += i;
+// };
 
 
-console.log(sumOfLoop);
+// console.log(sumOfLoop);
 
+
+//for in - objects, 
+//for of - arrays
+
+// const obj1 = {
+//     prop1: 1,
+//     prop2: 2,
+//     prop3: 'value3'
+// }
+// for (let prop in obj1) {
+//     console.log(prop)
+// }
+
+
+const obj1 = {
+    prop1: 1,
+    prop2: 2,
+    prop3: 'value3'
+}
+for (let prop in obj1) {
+    console.log(`${obj1[prop]}`)
+    if (`${obj1[prop]}` == 'New') {
+        console.log()
+    }
+}
+
+const arr1 = ['valArr1', 'valArr2', 'valArr3']
+
+for (valueOfArr of arr1) {
+    console.log(valueOfArr)
+}
