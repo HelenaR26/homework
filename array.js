b777eec45c907f3963452ee277b3f0c0c9715f57
Elena <you@example.com>
2023-09-07 19:40:26 +0300

    added_2d_array

--- array.js
+++ array.js
@@ -34,3 +34,18 @@ for (let i  = 0; i < baseArr.length; i++) {
 console.log(resultWithUpperC)   
 }
 getOnlyString(arr1)
+
+let mArr22 = [
+    [1, 3, 'Shrek'],
+    [4, 10],
+    [40, 50]
+]
+
+let nameShrek = mArr22[0] [2]
+console.log(nameShrek)
+for (let i = 0; i < mArr22.length; i++) {
+    let smallArr = mArr22[i]
+    for (let y = 0; y < smallArr.length; y++) {
+        console.log("all array [" + i + "][" + y + "] = " + smallArr[y])
+    }
+}
