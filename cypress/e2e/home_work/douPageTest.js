66e61feba92599beac32fe74bd73d5f43d7f73d0
Elena <you@example.com>
2023-10-08 00:22:55 +0300

    added_pageobject_homework

---
+++ cypress/e2e/home_work/douPageTest.js
@@ -0,0 +1,22 @@
+class DouPage {
+    visit() {
+  
+      cy.visit('https://dou.ua/')
+    
+    }
+  
+    getLogo() {
+      return cy.get('.b-head .logo')
+    }
+  
+    getJobsButton() {
+      return cy.contains('Робота')
+    }
+  
+    getSearchInput() {
+      return cy.get('.search .inp')
+    }
+  
+  }
+  
+  export default new DouPage()
