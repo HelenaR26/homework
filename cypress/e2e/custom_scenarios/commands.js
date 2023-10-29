9c25063bbc3864c3866b6431982b8efdacdc8eb1
Elena <you@example.com>
2023-10-30 00:52:41 +0200

    added_some_files

---
+++ cypress/e2e/custom_scenarios/commands.js
@@ -0,0 +1,103 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('loginAndSetCookie', (url, cookieN, cookieV) => {
+  cy.visit(url)
+  cy.setCookie(cookieN, cookieV)
+})
+
+Cypress.Commands.add('loginAndSetLocalStorage', () => {
+  cy.request({
+    method: 'POST',
+    url: '/api/auth/login',
+    body: {
+      email: 'email@dmytro.com',
+      password: 'abc123',
+    },
+  }).then((response) => {
+    const accessToken = response.body.token;
+    // Store the access token in local storage
+    localStorage.setItem('auth-token', accessToken)
+  })
+})
+
+Cypress.Commands.add('createCategoryAndPosition', (categoryName, positionName, positionPrice) => {
+  const accessToken = window.localStorage.getItem('auth-token')
+  // First request to get the list of categories
+  cy.request({
+    method: 'GET',
+    url: 'api/categories',
+    headers: {
+      authorization: accessToken,
+    },
+  }).then((response) => {
+    const categories = response.body // Get all categories
+
+    let existingCategory;
+    for (const categoryId in categories) {
+      if (categories[categoryId].name === categoryName) {
+        existingCategory = categories[categoryId]
+        break
+      }
+    }
+
+    if (!existingCategory) {
+      // No category with this name, create one
+      cy.request({
+        method: 'POST',
+        url: 'api/category',
+        body: {
+          name: categoryName,
+        },
+        headers: {
+          authorization: accessToken,
+        },
+      }).then((categoryResponse) => {
+        const categoryId = categoryResponse.body._id;
+        cy.log(`Created category with ID: ${categoryId}`)
+
+        // Create a position inside the category
+        cy.request({
+          method: 'POST',
+          url: 'api/position',
+          body: {
+            category: categoryId,
+            cost: positionPrice,
+            name: positionName,
+          },
+          headers: {
+            authorization: accessToken,
+          },
+        }).then((positionResponse) => {
+          const positionId = positionResponse.body._id;
+          cy.log(`Created position with ID: ${positionId}`)
+        })
+      })
+    }
+  })
+})
+
+
---
+++ cypress/support/homework_request.cy.js
@@ -0,0 +1,19 @@
+describe('Login with API request', () => {
+    beforeEach(() => {
+      cy.loginAndSetLocalStorage()
+    })
+  
+    it('Send login data', () => {
+      cy.visit('/overview');
+      cy.get('div.card-content', { timeout: 5000 }).eq(0).should('be.visible')
+    })
+  
+    it('Create category and position', () => {
+      cy.createCategoryAndPosition('Test Category from Elena', 'New Position', 50, { timeout: 5000 })
+      cy.visit('/categories', { timeout: 5000 })
+      cy.scrollTo('bottom', { duration: 3000 })
+      cy.get('.collection-item').contains('Test Category from Elena').click({ force: true })
+      cy.wait(5000)
+      cy.contains('New Position').should('be.visible')
+    })
+  })
